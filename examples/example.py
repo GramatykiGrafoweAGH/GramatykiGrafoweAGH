7895108e5d9b528f587f3cf90ae27de84a166034
Mateusz Hurbol <mateusz.hurbol42@gmail.com>
2021-12-15 00:42:24 +0100

    remove unwanted change

--- examples/example.py
+++ examples/example.py
@@ -2,6 +2,6 @@ from GramatykiGrafoweAGH.project.task1 import make_initial_graph, P1, P2
 
 if __name__ == '__main__':
     G = make_initial_graph()
-    G.apply_productions([P1, P2, P2])
+    G.apply_productions([P1, P2])
     G.assert_no_duplicated_nodes()
     G.show()
