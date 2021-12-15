e3111b746638c0f35431a6de3061be2586331891
Mateusz Hurbol <mateusz.hurbol42@gmail.com>
2021-12-15 15:43:34 +0100

    Test 6 p1 (#13)
    * Test 1 P1
    
    * remove unwanted change
    
    * fix linter
    
    * Second test for P1
    
    * linter fix
    
    * Next tests
    
    * linter fix
    
    * Apply subgraph test
    
    * Fix linter
    
    * Dont modify if not applied
    
    * Fix linter
    
    * Test 1 P1
    
    * Fix linter
    
    * Test 1 P1

--- tests/project/test_task1.py
+++ tests/project/test_task1.py
@@ -1,6 +1,10 @@
-from GramatykiGrafoweAGH.project.task1 import make_initial_graph, P2
+from GramatykiGrafoweAGH.project.task1 import P2
+from GramatykiGrafoweAGH.project.utils import is_node_between
 from GramatykiGrafoweAGH.testing import assert_production_cannot_be_applied
-from GramatykiGrafoweAGH import Node, Graph
+from typing import List
+
+from GramatykiGrafoweAGH import Graph, Node
+from GramatykiGrafoweAGH.project.task1 import make_initial_graph, P1
 
 
 def make_P2_left_side_graph():
@@ -111,6 +115,140 @@ def test_make_initial_graph():
 
 def test_P1():
     pass
+
+
+def check_if_production_applied(G: Graph, e: Node):
+    Is = list(filter(lambda n: n.level == 1 and n.label == "I", G.neighbors(e)))
+
+    assert len(Is) == 1
+    I = Is[0]
+
+    Es = list(filter(lambda n: n.level == 1, G.neighbors(I)))
+    assert len(Es) == 4
+
+    check_square(G, Es, I)
+
+
+def check_square(G: Graph, Es: List[Node], I: Node):
+    for E in Es:
+        assert E in G.neighbors(I)
+
+    level_0 = list(filter(lambda n: n.level == 0 and n.label == "e", G.nodes))
+    assert len(level_0) == 1
+
+    for E in Es:
+        for neighbour in G.neighbors(E):
+            if neighbour.label == "E":
+                assert neighbour in Es
+                assert bool(neighbour.x == E.x) != bool(neighbour.y == E.y)
+            else:
+                assert neighbour.label == "I"
+                E_not_neighbours = [E1 for E1 in Es if E1 not in G.neighbors(E) and E1 != E]
+                assert len(E_not_neighbours) == 1
+                assert is_node_between(E_not_neighbours[0], neighbour, E)
+
+
+def test_P1_isomorphic():
+    G = Graph()
+    original_E = Node(label='E', x=0.5, y=0.5, level=0)
+    G.add_node(original_E)
+
+    P1(G)
+
+    assert len(G.nodes) == 6
+
+    level_0 = list(filter(lambda n: n.level == 0, G.nodes))
+    assert len(level_0) == 1
+    e = level_0[0]
+    assert e.label == "e"
+
+    level_1 = list(filter(lambda n: n.level == 1, G.nodes))
+    assert len(level_1) == 5
+
+    Es = [n for n in level_1 if n.label == "E"]
+    Is = [n for n in level_1 if n.label == "I"]
+
+    assert len(Es) == 4
+    assert len(Is) == 1
+
+    I: Node = Is[0]
+
+    check_square(G, Es, I)
+
+
+def test_P1_vertex_removed():
+    """Empty graph"""
+    G = Graph()
+
+    assert G.number_of_nodes == 0
+
+    assert_production_cannot_be_applied(P1, G)
+
+
+def test_P1_edge_removed():
+    """Inapplicable"""
+    pass
+
+
+def test_P1_label_changed():
+    """small letter e"""
+    G = Graph()
+    e = Node(label='e', x=0.5, y=0.5, level=0)
+    G.add_node(e)
+
+    assert_production_cannot_be_applied(P1, G)
+
+
+def test_P1_subgraph():
+    G = Graph()
+    E = Node(label='E', x=0.5, y=0.5, level=0)
+    i0 = Node(label='i', x=2.0, y=2.0, level=0)
+    i1 = Node(label='i', x=-1.0, y=-1.0, level=0)
+
+    G.add_node(E)
+    G.add_node(i0)
+    G.add_node(i1)
+
+    G.add_edges([(i0, E), (E, i1)])
+    P1(G)
+
+    assert len(G.nodes) == 8
+
+    level_0 = list(filter(lambda n: n.level == 0, G.nodes))
+    assert len(level_0) == 3
+
+    es = list(filter(lambda n: n.label == "e", level_0))
+    assert len(es) == 1
+    e = es[0]
+
+    check_if_production_applied(G, e)
+
+
+def test_P1_subgraph_dont_apply():
+    def make_graph() -> Graph:
+        G = Graph()
+        I = Node(label='I', x=0.5, y=0.5, level=0)
+        i0 = Node(label='i', x=2.0, y=2.0, level=0)
+        i1 = Node(label='i', x=-1.0, y=-1.0, level=0)
+
+        G.add_node(I)
+        G.add_node(i0)
+        G.add_node(i1)
+
+        G.add_edges([(i0, I), (I, i1)])
+        return G
+
+    G = make_graph()
+    nodes = G.nodes
+
+    assert_production_cannot_be_applied(P1, G)
+
+    G1 = make_graph()
+
+    assert G.number_of_nodes == G1.number_of_nodes
+    assert G.number_of_edges == G1.number_of_edges
+
+    assert nodes == G.nodes
 
 
 def test_P2_isomorphic_left_side():
