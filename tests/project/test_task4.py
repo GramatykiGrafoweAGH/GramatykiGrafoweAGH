e6c6a1db705912298f5c8ac61a2524662034ce13
wiktor145 <viktor145@wp.pl>
2021-12-31 12:58:48 +0100

    Initial task 4 commit, add some tests idea

--- tests/project/test_task4.py
+++ tests/project/test_task4.py
@@ -2,5 +2,44 @@ import pytest
 
 
 @pytest.mark.skip(reason='not implemented yet')
-def test_P7():
+def test_P7_isomorphic():
+    # create graph that perfectly fits p7 definition
+    # try to apply p7 once
+    # check if applied (isomorphic?)
+    pass
+
+
+@pytest.mark.skip(reason='not implemented yet')
+def test_P7_can_be_applied_only_once():
+    # create graph that perfectly fits p7 definition
+    # try to apply p7 once
+    # try to apply once more
+    # exception should be thrown
+    pass
+
+
+@pytest.mark.skip(reason='not implemented yet')
+def test_P7_removed_edge():
+    # create graph that perfectly fits p7 definition
+    # remove random edge (propbably we should test removing from different levels)
+    # try to apply p7 once
+    # exception should be thrown
+    pass
+
+
+@pytest.mark.skip(reason='not implemented yet')
+def test_P7_removed_node():
+    # create graph that perfectly fits p7 definition
+    # remove random node (probably we should test removing from different levels)
+    # try to apply p7 once
+    # exception should be thrown
+    pass
+
+
+@pytest.mark.skip(reason='not implemented yet')
+def test_P7_different_label():
+    # create graph that perfectly fits p7 definition
+    # change random label to different one (probably we should test on different levels)
+    # try to apply p7 once
+    # exception should be thrown
     pass
