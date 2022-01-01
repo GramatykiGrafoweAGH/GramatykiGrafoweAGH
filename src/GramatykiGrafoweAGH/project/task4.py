1bb2e37bae50218cd3271330776baa02c67fec1c
wiktor145 <viktor145@wp.pl>
2022-01-01 18:20:49 +0100

    first suggestion of task 4 refactor

--- src/GramatykiGrafoweAGH/project/task4.py
+++ src/GramatykiGrafoweAGH/project/task4.py
@@ -8,22 +8,31 @@ from GramatykiGrafoweAGH.project.utils import is_node_between
 def P7(G: Graph) -> None:
     for E2s in G.get_duplicates_with_label('E'):
         for E2L, E2R in combinations(E2s, 2):
+            E1L_, E2L_, E3L_ = None, None, None
+            E1R_, E2R_, E3R_ = None, None, None
+
             E13Ls = G.get_neighbors_with_label(E2L, 'E')
             for E1L, E3L in combinations(E13Ls, 2):
                 if is_node_between(E1L, E2L, E3L):
-                    for E1R in G.get_duplicates_of(E1L):
-                        for E3R in G.get_duplicates_of(E3L):
-                            if is_node_between(E1R, E2R, E3R):
-                                for I12L in G.get_common_neighbors_with_label(E1L, E2L, 'I'):
-                                    for I23L in G.get_common_neighbors_with_label(E2L, E3L, 'I'):
-                                        for iL in G.get_common_neighbors_with_label(I12L, I23L, 'i'):
-                                            for I12R in G.get_common_neighbors_with_label(E1R, E2R, 'I'):
-                                                for I23R in G.get_common_neighbors_with_label(E2R, E3R, 'I'):
-                                                    for iR in G.get_common_neighbors_with_label(I12R, I23R, 'i'):
-                                                        for E in G.get_common_neighbors_with_label(iL, iR, 'E'):
-                                                            G.merge_two_nodes(E1L, E1R)
-                                                            G.merge_two_nodes(E2L, E2R)
-                                                            G.merge_two_nodes(E3L, E3R)
-                                                            return
+                    E1L_, E2L_, E3L_ = E1L, E2L, E3L
+
+            if E1L_:
+                for E1R in G.get_duplicates_of(E1L_):
+                    for E3R in G.get_duplicates_of(E3L_):
+                        if is_node_between(E1R, E2R, E3R):
+                            E1R_, E2R_, E3R_ = E1R, E2R, E3R
+
+            if E1L_ and E1R_:
+                for I12L in G.get_common_neighbors_with_label(E1L_, E2L_, 'I'):
+                    for I23L in G.get_common_neighbors_with_label(E2L_, E3L_, 'I'):
+                        for iL in G.get_common_neighbors_with_label(I12L, I23L, 'i'):
+                            for I12R in G.get_common_neighbors_with_label(E1R_, E2R_, 'I'):
+                                for I23R in G.get_common_neighbors_with_label(E2R_, E3R_, 'I'):
+                                    for iR in G.get_common_neighbors_with_label(I12R, I23R, 'i'):
+                                        for E in G.get_common_neighbors_with_label(iL, iR, 'E'):
+                                            G.merge_two_nodes(E1L_, E1R_)
+                                            G.merge_two_nodes(E2L_, E2R_)
+                                            G.merge_two_nodes(E3L_, E3R_)
+                                            return
 
     raise CannotApplyProductionError()
