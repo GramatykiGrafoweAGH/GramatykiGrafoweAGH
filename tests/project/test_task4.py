11ecd4b78738d4f024da387afe6d8669e3217cf0
wiktor145 <viktor145@wp.pl>
2022-01-05 16:52:12 +0100

    add test for middle node not in the middle

--- tests/project/test_task4.py
+++ tests/project/test_task4.py
@@ -38,6 +38,41 @@ def make_P7_left_side_graph():
     return G
 
 
+def make_P7_left_side_graph_not_middle():
+    G = Graph()
+
+    level = 2
+
+    E = Node(label='E', x=0, y=0, level=level)
+    iL = Node(label='i', x=-1, y=-1, level=level)
+    iR = Node(label='i', x=1, y=-1, level=level)
+
+    I1L = Node(label='I', x=-2, y=-2, level=level + 1)
+    I2L = Node(label='I', x=-1, y=-2, level=level + 1)
+
+    I1R = Node(label='I', x=1, y=-2, level=level + 1)
+    I2R = Node(label='I', x=2, y=-2, level=level + 1)
+
+    E1L = Node(label='E', x=0, y=-3, level=level + 1)
+    E2L = Node(label='E', x=0, y=-4.2, level=level + 1)
+    E3L = Node(label='E', x=0, y=-5, level=level + 1)
+
+    E1R = Node(label='E', x=0, y=-3, level=level + 1)
+    E2R = Node(label='E', x=0, y=-4.2, level=level + 1)
+    E3R = Node(label='E', x=0, y=-5, level=level + 1)
+
+    G.add_nodes([E, iL, iR, I1L, I2L, I1R, I2R, E1L, E2L, E3L, E1R, E2R, E3R])
+
+    G.add_edges([
+        (E, iL), (E, iR), (iL, I1L), (iL, I2L), (iR, I1R), (iR, I2R),
+        (I1L, E1L), (I1L, E2L), (I2L, E2L), (I2L, E3L),
+        (I1R, E1R), (I1R, E2R), (I2R, E2R), (I2R, E3R),
+        (E1L, E2L), (E2L, E3L), (E1R, E2R), (E2R, E3R)
+    ])
+
+    return G
+
+
 def make_P7_right_side_graph():
     G = Graph()
 
@@ -161,4 +196,9 @@ def test_P7_on_P8_graph():
     E1L, E1R = G.get_nodes_with_predicate(lambda n: n.label == "E" and n.x == 0 and n.y == -3 and n.level == 3)
     G.merge_two_nodes(E1L, E1R)
 
+    assert_production_cannot_be_applied(P7, G)
+
+
+def test_P7_left_side_not_middle():
+    G = make_P7_left_side_graph_not_middle()
     assert_production_cannot_be_applied(P7, G)
