791313d206b287e65aa2f5d766b8a1fd714570ac
jakubdybczak <jakub.dybczak@gmail.com>
2022-01-09 18:52:52 +0100

    Add 4 new tests

--- tests/project/test_task2.py
+++ tests/project/test_task2.py
@@ -1,17 +1,31 @@
 from GramatykiGrafoweAGH import Graph, Node
 from GramatykiGrafoweAGH.project.task2 import P3, P4
 from GramatykiGrafoweAGH.testing import assert_production_cannot_be_applied
+from iteration_utilities import deepflatten
 
 
-def make_initial_graph_P3() -> Graph:
+def get_test_node_positions(positions_modifier):
+    positions = [[[-0.5, -.5], [0.0, -.5], [0.5, -.5]],
+                 [[-0.5, 0.0], [0.0, 0.0], [0.5, 0.0]],
+                 [[-0.5, 0.5], [0.0, 0.5], [0.5, 0.5]]]
+
+    positions_modifier(positions)
+    return deepflatten(positions)
+
+
+def make_initial_graph_P3(positions_modifier=lambda x: x) -> Graph:
     G = Graph()
 
-    I = Node(label='I', x=0, y=0, level=1)
-    E00 = Node(label='E', x=-.5, y=-.5, level=1)
-    E20 = Node(label='E', x=-.5, y=.5, level=1)
-    E02 = Node(label='E', x=.5, y=-.5, level=1)
-    E22 = Node(label='E', x=.5, y=.5, level=1)
-    E10 = Node(label='E', x=-.5, y=0, level=1)
+    (x00, y00, x01, y01, x02, y02,
+     x10, y10, x11, y11, x12, y12,
+     x20, y20, x21, y21, x22, y22) = get_test_node_positions(positions_modifier)
+
+    I = Node(label='I', x=x11, y=y11, level=1)
+    E00 = Node(label='E', x=x00, y=y00, level=1)
+    E20 = Node(label='E', x=x20, y=y20, level=1)
+    E02 = Node(label='E', x=x02, y=y02, level=1)
+    E22 = Node(label='E', x=x22, y=y22, level=1)
+    E10 = Node(label='E', x=x10, y=y10, level=1)
 
     G.add_nodes([I, E00, E02, E20, E22, E10])
 
@@ -26,16 +40,20 @@ def make_initial_graph_P3() -> Graph:
     return G
 
 
-def make_initial_graph_P4() -> Graph:
+def make_initial_graph_P4(positions_modifier=lambda x: x) -> Graph:
     G = Graph()
 
-    I = Node(label='I', x=0, y=0, level=1)
-    E00 = Node(label='E', x=-.5, y=-.5, level=1)
-    E20 = Node(label='E', x=-.5, y=.5, level=1)
-    E02 = Node(label='E', x=.5, y=-.5, level=1)
-    E22 = Node(label='E', x=.5, y=.5, level=1)
-    E10 = Node(label='E', x=-.5, y=0, level=1)
-    E21 = Node(label='E', x=0, y=.5, level=1)
+    (x00, y00, x01, y01, x02, y02,
+     x10, y10, x11, y11, x12, y12,
+     x20, y20, x21, y21, x22, y22) = get_test_node_positions(positions_modifier)
+
+    I = Node(label='I', x=x11, y=y11, level=1)
+    E00 = Node(label='E', x=x00, y=y00, level=1)
+    E20 = Node(label='E', x=x20, y=y20, level=1)
+    E02 = Node(label='E', x=x02, y=y02, level=1)
+    E22 = Node(label='E', x=x22, y=y22, level=1)
+    E10 = Node(label='E', x=x10, y=y10, level=1)
+    E21 = Node(label='E', x=x21, y=y21, level=1)
 
     G.add_nodes([I, E00, E02, E20, E22, E21, E10])
 
@@ -50,20 +68,14 @@ def make_initial_graph_P4() -> Graph:
     return G
 
 
-def make_right_side_graph_P3():
+def make_right_side_graph_P3(positions_modifier=lambda x: x):
     G = Graph()
 
     level = 1
 
-    x00, y00 = -0.5, -0.5
-    x01, y01 = 0.0, -0.5
-    x02, y02 = 0.5, -0.5
-    x10, y10 = -0.5, 0.0
-    x11, y11 = 0.0, 0.0
-    x12, y12 = 0.5, 0.0
-    x20, y20 = -0.5, 0.5
-    x21, y21 = 0.0, 0.5
-    x22, y22 = 0.5, 0.5
+    (x00, y00, x01, y01, x02, y02,
+     x10, y10, x11, y11, x12, y12,
+     x20, y20, x21, y21, x22, y22) = get_test_node_positions(positions_modifier)
 
     i = Node(label='i', x=x11, y=y11, level=level)
     E00 = Node(label='E', x=x00, y=y00, level=level)
@@ -83,10 +95,14 @@ def make_right_side_graph_P3():
         (E20, E22),
     ])
 
-    I00 = Node(label='I', x=(x00 + x11) / 2, y=(y00 + y11) / 2, level=level + 1)
-    I01 = Node(label='I', x=(x02 + x11) / 2, y=(y02 + y11) / 2, level=level + 1)
-    I10 = Node(label='I', x=(x20 + x11) / 2, y=(y20 + y11) / 2, level=level + 1)
-    I11 = Node(label='I', x=(x22 + x11) / 2, y=(y22 + y11) / 2, level=level + 1)
+    I00 = Node(label='I', x=(x00 + x11 + x01 + x10) / 4,
+               y=(y00 + y11 + y01 + y10) / 4, level=level + 1)
+    I01 = Node(label='I', x=(x02 + x11 + x01 + x12) / 4,
+               y=(y02 + y11 + y01 + y12) / 4, level=level + 1)
+    I10 = Node(label='I', x=(x20 + x11 + x21 + x10) / 4,
+               y=(y20 + y11 + y21 + y10) / 4, level=level + 1)
+    I11 = Node(label='I', x=(x22 + x11 + x21 + x12) / 4,
+               y=(y22 + y11 + y21 + y12) / 4, level=level + 1)
 
     E00 = Node(label='E', x=x00, y=y00, level=level + 1)
     E01 = Node(label='E', x=x01, y=y01, level=level + 1)
@@ -124,20 +140,14 @@ def make_right_side_graph_P3():
     return G
 
 
-def make_right_side_graph_P4():
+def make_right_side_graph_P4(positions_modifier=lambda x: x):
     G = Graph()
 
     level = 1
 
-    x00, y00 = -0.5, -0.5
-    x01, y01 = 0.0, -0.5
-    x02, y02 = 0.5, -0.5
-    x10, y10 = -0.5, 0.0
-    x11, y11 = 0.0, 0.0
-    x12, y12 = 0.5, 0.0
-    x20, y20 = -0.5, 0.5
-    x21, y21 = 0.0, 0.5
-    x22, y22 = 0.5, 0.5
+    (x00, y00, x01, y01, x02, y02,
+     x10, y10, x11, y11, x12, y12,
+     x20, y20, x21, y21, x22, y22) = get_test_node_positions(positions_modifier)
 
     i = Node(label='i', x=x11, y=y11, level=level)
     E00 = Node(label='E', x=x00, y=y00, level=level)
@@ -158,10 +168,14 @@ def make_right_side_graph_P4():
         (E20, E21), (E21, E22),
     ])
 
-    I00 = Node(label='I', x=(x00 + x11) / 2, y=(y00 + y11) / 2, level=level + 1)
-    I01 = Node(label='I', x=(x02 + x11) / 2, y=(y02 + y11) / 2, level=level + 1)
-    I10 = Node(label='I', x=(x20 + x11) / 2, y=(y20 + y11) / 2, level=level + 1)
-    I11 = Node(label='I', x=(x22 + x11) / 2, y=(y22 + y11) / 2, level=level + 1)
+    I00 = Node(label='I', x=(x00 + x11 + x01 + x10) / 4,
+               y=(y00 + y11 + y01 + y10) / 4, level=level + 1)
+    I01 = Node(label='I', x=(x02 + x11 + x01 + x12) / 4,
+               y=(y02 + y11 + y01 + y12) / 4, level=level + 1)
+    I10 = Node(label='I', x=(x20 + x11 + x21 + x10) / 4,
+               y=(y20 + y11 + y21 + y10) / 4, level=level + 1)
+    I11 = Node(label='I', x=(x22 + x11 + x21 + x12) / 4,
+               y=(y22 + y11 + y21 + y12) / 4, level=level + 1)
 
     E00 = Node(label='E', x=x00, y=y00, level=level + 1)
     E01 = Node(label='E', x=x01, y=y01, level=level + 1)
@@ -197,6 +211,32 @@ def make_right_side_graph_P4():
     ])
 
     return G
+
+
+def test_P3_wrong_midpoint():
+    def positions_modifier(positions):
+        positions[1][0][0] = positions[1][0][0] - 1
+
+    G = make_initial_graph_P3(positions_modifier)
+    assert_production_cannot_be_applied(P3, G)
+
+
+def test_P3_isomorphic_after_changed_midpoint():
+    # test if production is correct after moving bottom left point (and corresponding midpoints)
+    def positions_modifier(positions):
+        positions[0][0][0] -= 0.5
+        positions[0][0][1] -= 0.5
+        positions[1][0][0] -= 0.25
+        positions[0][1][0] -= 0.25
+        positions[1][0][1] -= 0.25
+        positions[0][1][1] -= 0.25
+
+    G = make_initial_graph_P3(positions_modifier)
+    expected = make_right_side_graph_P3(positions_modifier)
+
+    P3(G)
+
+    assert G.is_isomorphic_with(expected)
 
 
 def test_P3_isomorphic_left_side():
@@ -270,6 +310,33 @@ def test_P3_left_side_invariance():
     remove_node(expected)
 
     assert_production_cannot_be_applied(P3, G)
+    assert G.is_isomorphic_with(expected)
+
+
+def test_P4_wrong_midpoint():
+    def positions_modifier(positions):
+        positions[2][1][1] = positions[2][1][1] + 1
+
+    G = make_initial_graph_P4(positions_modifier)
+
+    assert_production_cannot_be_applied(P4, G)
+
+
+def test_P4_isomorphic_after_changed_midpoint():
+    # test if production is correct after moving top left point (and corresponding midpoints)
+    def positions_modifier(positions):
+        positions[2][0][0] -= 0.5
+        positions[2][0][1] += 0.5
+        positions[1][0][0] -= 0.25
+        positions[2][1][0] -= 0.25
+        positions[1][0][1] += 0.25
+        positions[2][1][1] += 0.25
+
+    G = make_initial_graph_P4(positions_modifier)
+    expected = make_right_side_graph_P4(positions_modifier)
+
+    P4(G)
+
     assert G.is_isomorphic_with(expected)
 
 
